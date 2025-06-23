842940703e1d8a06d3f23c59f920ce72b9aeed6f
Alexander Ivanov <rubtowner@atomicmail.io>
2025-06-23 13:54:44 +0300

    Mobile devices support

---
+++ assets/exchange-4Q0R93eZ.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,r,E as s}from"./Exchange-9Fh7Ca6E.js";t.createRoot(document.getElementsByTagName("body")[0]).render(e.jsx(r.StrictMode,{children:e.jsx(s,{})}));
--- assets/exchange-Duo-f3HG.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,r,E as s}from"./Exchange-BuPfzeS7.js";t.createRoot(document.getElementsByTagName("body")[0]).render(e.jsx(r.StrictMode,{children:e.jsx(s,{})}));
