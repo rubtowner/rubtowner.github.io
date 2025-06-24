755b38ed7c05dcc2ea72efcd25a50a453c13f560
Alexander Ivanov <rubtowner@atomicmail.io>
2025-06-25 00:47:28 +0300

    Improve UI

---
+++ assets/exchange-1tsYRAwr.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,r,E as s}from"./Exchange-BbU1FRRu.js";t.createRoot(document.getElementsByTagName("body")[0]).render(e.jsx(r.StrictMode,{children:e.jsx(s,{})}));
--- assets/exchange-Dm_uPkJ0.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,r,E as s}from"./Exchange-DgJS4rni.js";t.createRoot(document.getElementsByTagName("body")[0]).render(e.jsx(r.StrictMode,{children:e.jsx(s,{})}));
